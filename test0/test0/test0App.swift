1a8d6e35aa2af598ef152cbdf564afbc4f30cbe7
baigubulu <1562025956@qq.com>
2022-01-12 16:19:43 +0800

    fisrt commit

---
+++ test0/test0/test0App.swift
@@ -0,0 +1,17 @@
+//
+//  test0App.swift
+//  test0
+//
+//  Created by yanwei on 2022/1/12.
+//
+
+import SwiftUI
+
+@main
+struct test0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
